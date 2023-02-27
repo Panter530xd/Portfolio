d163fde7fa30b351bd424aff20afb20e3535a198
Boban Senic <bobansenic@gmail.com>
2023-02-27 19:25:12 +0100

    I added some staff

--- fullstack-next-typescript/app/layout.jsx
+++ fullstack-next-typescript/app/layout.jsx
@@ -1,17 +1,25 @@
-import "./globals.css";
-import Nav from "./auth/Nav";
-import QueryWrapper from "./auth/QueryWrapper";
+import "./globals.css"
+import Nav from "./auth/Nav"
+import QueryWrapper from "./auth/QueryWrapper"
 
-export default function Layout({children}) {
+
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
       <head />
-      <body>
+      <body
+        className={`mx-4 md:mx-48 xl:mx-96  font-sans bg-gray-200`}
+      >
         <QueryWrapper>
-        <Nav />
-        {children}
+            <Nav />
+            {children}
         </QueryWrapper>
       </body>
     </html>
-  );
+  )
 }
